972c350e77b7d7c73b302cb3431c8b73d436778f
NiNejah <almawlanejeh@gmail.com>
2025-02-28 23:52:53 +0100

    fix: project title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'action-time-line-2'`, () => {
+  it(`should have as title 'action-time-line'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('action-time-line-2');
+    expect(app.title).toEqual('action-time-line');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, action-time-line-2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, action-time-line');
   });
 });
